4ff65c6df491eeb302d9d2e2e776c504c4f0bace
Lion-gao <47811197+Lion-gao@users.noreply.github.com>
2021-10-20 16:52:22 +0900

    Update ruby-setup.sh

--- ruby-setup.sh
+++ ruby-setup.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 
 printf "install: --no-rdoc --no-ri\nupdate:  --no-rdoc --no-ri\n" >> ~/.gemrc
-gem install rails -v 5.2.0
 sudo service mysqld stop
 sudo yum -y erase mysql-config mysql55-server mysql55-libs mysql55
 sudo yum -y install mysql57-server mysql57
 sudo service mysqld start
 sudo chkconfig mysqld on
 sudo yum install -y mysql-devel
+
+cd ~
+git clone git://github.com/sstephenson/rbenv.git .rbenv
+git clone git://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
+echo 'export PATH="$HOME/.rbenv/bin:$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> ~/.bash_profile
+echo 'eval "$(rbenv init -)"' >> ~/.bash_profile
+source ~/.bash_profile
+
+export CONFIGURE_OPTS="--disable-install-doc --disable-install-rdoc"
+rbenv install -v 2.7.3
+unset CONFIGURE_OPTS
+
+rbenv global 2.7.3
+gem install rails -v 5.2.0
