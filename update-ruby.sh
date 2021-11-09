de1a4b5f02bb697984cc0963961f3746ed449376
Lion-gao <47811197+Lion-gao@users.noreply.github.com>
2021-11-09 18:29:56 +0900

    4tab to 2tab & add empty lines

--- update-ruby.sh
+++ update-ruby.sh
@@ -2,21 +2,23 @@
 
 cd ~
 
+
 # uninstall rvm
 if command -v rvm &> /dev/null; then
-    rvm seppuku --force
-    source ~/.bash_profile
+  rvm seppuku --force
+  source ~/.bash_profile
 fi
 
 
 # install rbenv
 if ! command -v rbenv &> /dev/null; then
-    git clone git://github.com/sstephenson/rbenv.git .rbenv
-    git clone git://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
-    echo 'export PATH="$HOME/.rbenv/bin:$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> ~/.bash_profile
-    echo 'eval "$(rbenv init -)"' >> ~/.bash_profile
-    source ~/.bash_profile
+  git clone git://github.com/sstephenson/rbenv.git .rbenv
+  git clone git://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
+  echo 'export PATH="$HOME/.rbenv/bin:$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> ~/.bash_profile
+  echo 'eval "$(rbenv init -)"' >> ~/.bash_profile
+  source ~/.bash_profile
 fi
+
 
 # install ruby v2.7.3
 rbenv install 2.7.3 -s
@@ -27,9 +29,11 @@ cd ~/environment/pictgram
 rbenv local 2.7.3
 sed -e "s/ruby .*/ruby '2.7.3'/" -i Gemfile
 
+
 # install bundler
 gem install bundler -v `tail -n1 Gemfile.lock`
 bundle install
+
 
 # commit
 git reset
