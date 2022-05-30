b101ecf4f686f1c99af34980e9a2716325c86919
Lion-gao <47811197+Lion-gao@users.noreply.github.com>
2022-05-31 05:12:16 +0900

    replace with published content

--- php-setup.sh
+++ php-setup.sh
@@ -1,19 +1,29 @@
 #!/bin/bash
 
-sudo yum update -y     
-
-sudo service mysqld stop
-sudo yum -y erase mysql-config mysql55-server mysql55-libs mysql55
-sudo yum -y install mysql57-server mysql57
-sudo service mysqld start
-sudo chkconfig mysqld on
-
-sudo yum -y install php73 php73-mbstring php73-pdo php73-mysqlnd
-sudo alternatives --set php /usr/bin/php-7.3
+sudo yum update -y
+# mysql
+sudo service mariadb stop
+sudo yum -y erase mariadb-config mariadb-common mariadb-libs mariadb
+sudo yum localinstall https://dev.mysql.com/get/mysql80-community-release-el7-1.noarch.rpm -y
+sudo yum-config-manager --disable mysql80-community
+sudo yum-config-manager --enable mysql57-community
+sudo rpm --import https://repo.mysql.com/RPM-GPG-KEY-mysql-2022
+sudo yum install mysql-community-server -y
+sudo systemctl start mysqld.service
+sudo systemctl enable mysqld.service
+# php
+sudo yum remove -y php php-*
+sudo amazon-linux-extras disable lamp-mariadb10.2-php7.2
+sudo amazon-linux-extras install php8.0 -y
+sudo amazon-linux-extras enable php8.0
+sudo yum install php-mbstring php-pdo php-mysqlnd php-bcmath php-xml -y
+sudo rm -f /etc/php.d/30-xdebug.ini
+# composer
 curl -sS https://getcomposer.org/installer | php
 sudo mv composer.phar /usr/bin/composer
+# heroku
 curl -OL https://cli-assets.heroku.com/heroku-linux-x64.tar.gz
 tar zxf heroku-linux-x64.tar.gz && rm -f heroku-linux-x64.tar.gz
 sudo mv heroku /usr/local
-echo 'PATH=/usr/local/heroku/bin:$PATH' >> $HOME/.bash_profile
-source $HOME/.bash_profile > /dev/null
+echo 'export PATH=/usr/local/heroku/bin:$PATH' >> $HOME/.bash_profile
+source $HOME/.bash_profile
