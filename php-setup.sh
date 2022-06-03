059ef56ad704d6bb581da95ad833d5379b92b76e
Lion-gao <47811197+Lion-gao@users.noreply.github.com>
2022-06-03 16:45:19 +0900

    separate post-processing and add comments

--- php-setup.sh
+++ php-setup.sh
@@ -39,4 +39,7 @@ curl -OL https://cli-assets.heroku.com/heroku-linux-x64.tar.gz
 tar zxf heroku-linux-x64.tar.gz && rm -f heroku-linux-x64.tar.gz
 sudo mv heroku /usr/local
 echo 'export PATH=/usr/local/heroku/bin:$PATH' >> $HOME/.bash_profile
+
+
+# reload shell config
 source $HOME/.bash_profile
